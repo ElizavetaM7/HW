3ea0bee0d974bd9d4a7bab0ed685304b3195e494
Elizaveta.Mamzeleva@urfu.me <Elizaveta.Mamzeleva@urfu.me>
2021-12-28 21:24:38 +0500

    Задание 7

---
+++ Task7/Task7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine(f(x));
+            Console.ReadKey();
+        }
+        static double f(double x)
+        {
+            double f;
+            if (x < -3)
+            {
+                f = 0;
+                return f;
+            }
+            else
+            {
+                if (x > -1)
+                {
+                    f = 2;                    
+                }
+                else
+                {
+                    f = 1;
+                }
+                return f;
+            }
+        }
+    }
+}
---
+++ Task9/Task9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = 0;
+            m++;
+            int a = (2 * m - 1) / (2 * m + 1);
+            int[] num = new int[n];
+
+            Console.WriteLine(num);
+
+            Console.ReadKey();
+        }
+    }
+}
