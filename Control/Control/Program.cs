98eeec009daf2a2fd745b7a766b698ab26670a02
Elizaveta.Mamzeleva@urfu.me <Elizaveta.Mamzeleva@urfu.me>
2021-12-30 14:14:28 +0500

    Попытка генерации ключа

---
+++ Control/Control/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+            text = text.ToUpper();
+            string alph = "абвгдежзийклмнопрстуфхцчшыьэюя";
+            string key;
+            int dl = text.Length;
+
+            Console.ReadKey();
+
+            string alph2 = "abcdefghijklmnopqrstuvwxyz";
+
+            var d = new char[dl];
+            for (int i = 0; 1 < d.Length; i++) ;
+            {
+
+            }
+        }
+        static void Shifr()
+        {
+
+
+        }
+        
+    }
+}
